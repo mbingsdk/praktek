d3341a0990b8d299e6a7ca6bc83d820d6b855d7c
Tamin Robelcom <53684389+mbingsdk@users.noreply.github.com>
2021-11-24 18:59:45 +0800

    Update kasir.py

--- kasir.py
+++ kasir.py
@@ -1,74 +1,93 @@
 """
-PROGRAM KASIR
+PROGRAM KASIR SEDERHANA
 """
 
-#Database
-dataBarang = [
+#database barang
+barang = [
     {
         "id":0,
-        "nama":"Indomie",
+        "nama":"indomie",
         "harga":3000
     },
     {
         "id":1,
-        "nama":"Shampo",
-        "harga":1000
+        "nama":"Telur Ayam Ras",
+        "harga":2000
     }
 ]
 
-#Penambahan Barang
+#fungsi input data barang
 def addBarang():
-    print("<|| PENAMBAHAN DAFTAR BARANG")
-    namaBarang = input("Nama Barang: ")
-    hargaBarang = int(input("Harga: "))
-    totalBarang = len(dataBarang)
-    lastId = dataBarang[totalBarang-1]["id"]
-    data = {
-        "id":lastId+1,
-        "nama":namaBarang,
-        "harga":hargaBarang
-    }
-    dataBarang.append(data)
-    print("<|| Barang Berhasil Ditambahkan!")
+    nama = input("Masukkan nama barang: ")
+    harga = int(input("Masukkan harga barang : "))
+    print("\nPenabahan barang baru, ", nama, " yang harganya ", harga)
+    confirm = input("\nLanjut : (y/n) ")
+    if confirm == "y":
+        barang.append({
+            "id":barang[len(barang)-1]["id"]+1,
+            "nama":nama,
+            "harga":harga
+        })
+        print("\nDone Bos,...!!")
+    elif confirm == "n":
+        addBarang()
+    else:
+        exit()
 
-#Penghapusan Barang
-def hapusBarang():
+#fungsi hapus data barang
+def removeBarang():
     daftarBarang()
-    pilihan = int(input("\nPilih ID barang: "))
-    pilihan = [x for x in dataBarang if x["id"] == pilihan][0]
-    dataBarang.remove(pilihan)
-    print("Barang Berhasil Disingkirkan!")
+    select = int(input("\nMasukkan nomor ID barang : "))
+    barangTerpilih = [x for x in barang if x["id"] == select][0]
+    confirm = input(str(barangTerpilih["nama"]) + "akan dihapus? (y/n)")
+    if confirm == "y":
+        barang.remove(barangTerpilih)
+        print("Barang berhasil dihapus")
+    elif confirm == "y":
+        removeBarang()
+    else:
+        exit()
 
-#Menampilkan Data Barang
+#fungsi menampilkan data barang
 def daftarBarang():
-    print("\nID", " "*3, "Nama Barang", "Harga")
-    for i in range(len(dataBarang)):
-        print(dataBarang[i]["id"], " "*3, dataBarang[i]["nama"], dataBarang[i]["harga"])
+    print("\nID", " "*3, "Nama Barang")
+    for i in range(len(barang)):
+        print(barang[i]["id"], " "*3, barang[i]["nama"])
 
-#Data Belanja
+#fungsi menghitung data belanja
 def belanja():
-    daftarBarang()
-    idBarang = int(input("Pilih ID barang: "))
-    jumlahBarang = int(input("Mau beli berapa banyak: "))
-    detailBarang = [x for x in dataBarang if x["id"] == idBarang][0]
-    totalHarga = detailBarang["harga"] * jumlahBarang
-    print("Anda harus bayar sekarang atau saya bunuh, Totalnya : ", totalHarga)
+    print("\nMau beli apa,...??")
+    totalHarga = 0
+    while True:
+        daftarBarang()
+        print("*Kalo sudah cukup, ketik 99")
+        opsi = int(input("Masukkan ID barang: "))
+        if opsi == 99:
+            break
+        jumlahBarang = int(input("Mau beli berapa: "))
+        selected = [x for x in barang if x["id"] == opsi][0]
+        harga = selected["harga"]*jumlahBarang
+        totalHarga += harga
+        print(totalHarga)
+    print("\nTotal pembayaran adalah ", totalHarga)
 
-#Menunya
+#fungsi menu
 def menu():
-    a = ["Belanja", "Tambah Item", "Hapus Item"]
+    a = ["Belanja", "Tambahkan Barang", "Hapus Barang"]
+    print("\n-----MENU-----")
     for i in range(len(a)):
         print(i, a[i])
-    b = int(input("Pilih nomor opsi: "))
+    b = int(input("Pilih opsi: "))
     if b < len(a):
         if b == 0:
             belanja()
         elif b == 1:
             addBarang()
         elif b == 2:
-            hapusBarang()
+            removeBarang()
         else:
             exit()
 
+#run program
 while True:
     menu()
